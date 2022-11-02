9633ae5e40f92671bf34c57ae3942b02b4bd9544
Thomas Sand <sand.thomas.ms@gmail.com>
2022-11-02 22:21:17 +0100

    Formatierung
    - 'D:\Works\Workspaces\Stamps\stampgui\src\main\java\de\sand\main\myMain.java':
      - Einrückung korrigiert

--- stampgui/src/main/java/de/sand/main/myMain.java
+++ stampgui/src/main/java/de/sand/main/myMain.java
@@ -5,8 +5,8 @@ import de.sand.work.Caller;
 
 public class myMain {
 
-    static boolean gui = false;
-    static boolean vers = false;
+  static boolean gui = false;
+  static boolean vers = false;
   public static void main(String[] args) {
     if (args.length > 0) {
       for (int i = 0; i < args.length; i++) {
